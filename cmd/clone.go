4722064df653db1e2ff4c29b9509c1b811fa5a38
koki-develop <kou.pg.0131@gmail.com>
2024-01-12 21:23:23 +0900

    Implement clone command

--- cmd/clone.go
+++ cmd/clone.go
@@ -1,11 +1,31 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"strings"
+
+	"github.com/koki-develop/gh-q/internal/cli"
+	"github.com/spf13/cobra"
+)
 
 var cloneCmd = &cobra.Command{
 	Use:  "clone",
 	Args: cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		segs := strings.Split(args[0], "/")
+		if len(segs) != 2 {
+			return errors.New("invalid repository name")
+		}
+		owner, repo := segs[0], segs[1]
+
+		c, err := cli.NewClient()
+		if err != nil {
+			return err
+		}
+
+		if err := c.Clone(owner, repo); err != nil {
+			return err
+		}
 		return nil
 	},
 }
---
+++ internal/cli/clone.go
@@ -0,0 +1,25 @@
+package cli
+
+import (
+	"path/filepath"
+
+	"github.com/koki-develop/gh-q/internal/git"
+)
+
+func (c *Client) Clone(owner, repo string) error {
+	dest := filepath.Join(c.root, "github.com", owner, repo)
+
+	opts := []git.CloneOption{}
+	auth, err := c.Auth()
+	if err != nil {
+		return err
+	}
+	if auth != nil {
+		opts = append(opts, git.WithCloneAuth(auth.Username, auth.Token))
+	}
+
+	if err := c.gitClient.Clone(owner, repo, dest, opts...); err != nil {
+		return err
+	}
+	return nil
+}
