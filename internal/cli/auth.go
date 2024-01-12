8cb3b6216233fc28b3bc784a2133d35bfc4d4af8
koki-develop <kou.pg.0131@gmail.com>
2024-01-12 21:20:35 +0900

    Add authentication function to CLI client

---
+++ internal/cli/auth.go
@@ -0,0 +1,24 @@
+package cli
+
+import (
+	"github.com/cli/go-gh/v2/pkg/auth"
+	"github.com/koki-develop/gh-q/internal/git"
+)
+
+func (c *Client) Auth() (*git.Auth, error) {
+	h, _ := auth.DefaultHost()
+	tkn, _ := auth.TokenForHost(h)
+	if tkn == "" {
+		return nil, nil
+	}
+
+	viewer, err := c.gitClient.Viewer()
+	if err != nil {
+		return nil, err
+	}
+
+	return &git.Auth{
+		Username: viewer,
+		Token:    tkn,
+	}, nil
+}
