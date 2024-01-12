7c0c1a663ec0ef6c86ecb5ae7bfb22a7a416948c
koki-develop <kou.pg.0131@gmail.com>
2024-01-12 21:19:41 +0900

    Add root directory and gitconfig in cli client initialization

--- internal/cli/client.go
+++ internal/cli/client.go
@@ -1,13 +1,47 @@
 package cli
 
-import "github.com/koki-develop/gh-q/internal/git"
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/go-git/go-git/v5/config"
+	"github.com/koki-develop/gh-q/internal/git"
+)
 
 type Client struct {
+	root      string
 	gitClient *git.Client
 }
 
-func NewClient() *Client {
-	return &Client{
-		gitClient: git.NewClient(),
+func NewClient() (*Client, error) {
+	// load gitconfig
+	cfg, err := config.LoadConfig(config.GlobalScope)
+	if err != nil {
+		return nil, err
 	}
+
+	// load root directory
+	ghq := cfg.Raw.Section("ghq")
+	root := ghq.Option("root")
+	if root == "" {
+		// if not set, use `$HOME/ghq`
+		r, err := defaultRoot()
+		if err != nil {
+			return nil, err
+		}
+		root = r
+	}
+
+	return &Client{
+		root:      root,
+		gitClient: git.NewClient(),
+	}, nil
+}
+
+func defaultRoot() (string, error) {
+	h, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(h, "ghq"), nil
 }
