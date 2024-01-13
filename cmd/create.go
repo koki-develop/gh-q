f2d0d7b8175f449ef3f6eb6141aa0f8786028455
koki-develop <kou.pg.0131@gmail.com>
2024-01-13 09:36:16 +0900

    Update create command to support local repository creation

--- cmd/create.go
+++ cmd/create.go
@@ -2,17 +2,22 @@ package cmd
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
+	"github.com/fatih/color"
 	"github.com/koki-develop/gh-q/internal/cli"
 	"github.com/spf13/cobra"
 )
 
 var createCmd = &cobra.Command{
-	Use:     "create OWNER/REPO",
+	Use: fmt.Sprintf("create %s/%s|%s",
+		color.New(color.Italic).Sprint("OWNER"), color.New(color.Italic).Sprint("REPO"),
+		color.New(color.Italic).Sprint("REPO"),
+	),
 	Aliases: []string{"c"},
-	Short:   "Create a new repository",
-	Long:    "Create a new repository.",
+	Short:   "Create a new repository on local",
+	Long:    "Create a new repository on local.",
 	Args:    cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		c, err := cli.NewClient()
